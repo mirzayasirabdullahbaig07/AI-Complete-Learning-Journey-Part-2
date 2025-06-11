591e673712f6f131230d85e0b4e98cdc83512aa9
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-06-11 20:34:31 +0500

    35 python program added

--- 01_Python/1000-Python-Programs/Programs_For_Python.py
+++ 01_Python/1000-Python-Programs/Programs_For_Python.py
@@ -1,3 +1,4 @@
+
 # ============================================
 # A. Variables & Data Types (15 Questions)
 # Goal: Logic Building, DSA, and Python Mastery
@@ -134,3 +135,95 @@ print((a > b) or (b < c))   # True
 
 # NOT operator
 print(not(a > b))           # True
+
+# 21. Write a program to divide two numbers and round the result to 2 decimal places.
+num_div1 = int(input("Enter your first number: "))
+num_div2 = int(input("Enter your second number: "))
+print(round(num_div1 / num_div2, 2))
+
+# 22. Find the remainder without using the % operator.
+num_for_division = 101
+divisor = 6
+remainder = num_for_division - (divisor * (num_for_division // divisor))
+print("Remainder is:", remainder)
+
+# 23. Write a program that checks whether two strings are equal.
+str_one = "yasir"
+str_one1 = "ABDULLAH"
+if str_one == str_one1:
+    print("equal")
+else:
+    print("not equal")
+
+# 24. Check if the number is even using the modulus operator.
+number_even = int(input("Enter the number: "))
+if number_even % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+# 25. Write a program that calculates simple interest.
+principal = 10
+rate = 500
+time = 60
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest is:", simple_interest)
+
+# 26. Multiply two floating-point numbers.
+floating_point_num = 0.99
+floating_point_num1 = 2.43
+print(floating_point_num * floating_point_num1)
+
+# 27. Write a program to check if a number is positive, negative, or zero.
+number_checker_01 = int(input("Enter the number: "))
+if number_checker_01 > 0:
+    print("The number is positive")
+elif number_checker_01 < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+# 28. Convert minutes into hours using arithmetic.
+total_mins = 180
+print(total_mins / 60, "hours")
+
+# 29. Write a program to calculate the perimeter of a rectangle.
+width_of_rect = 10
+height_of_rect = 5
+print("Perimeter of rectangle is:", 2 * (width_of_rect + height_of_rect))
+
+# 30. Write a program to increase a salary by 10% using operators.
+total_salary = 500000
+increased_salary = total_salary + (total_salary * 10 / 100)
+print("Increased salary is:", increased_salary)
+
+# 31. Use exponent operator to calculate power.
+comp_num1 = 4
+comp_num2 = 3
+print(comp_num1 ** comp_num2)
+
+# 32. Write a program to find the average of three numbers.
+avg_num1 = 3
+avg_num2 = 4
+avg_num3 = 5
+avg = (avg_num1 + avg_num2 + avg_num3) / 3
+print("Average is:", avg)
+
+# 33. Write a program that finds the largest of two numbers using ternary operator.
+num_largest_finder = int(input("Enter first number: "))
+num_largest_finder1 = int(input("Enter second number: "))
+print("Largest number is:", num_largest_finder if num_largest_finder > num_largest_finder1 else num_largest_finder1)
+
+# 34. Check if a number is divisible by both 3 and 5.
+divisible_number = int(input("Enter the number: "))
+if divisible_number % 3 == 0 and divisible_number % 5 == 0:
+    print("The number is divisible by both 3 and 5")
+else:
+    print("The number is not divisible by both 3 and 5")
+
+# 35. Calculate the Body Mass Index (BMI) using given formula.
+weight = 65  # in kg
+height = 1.68  # in meters
+bmi = weight / (height ** 2)
+print("BMI is:", round(bmi, 2))
+
