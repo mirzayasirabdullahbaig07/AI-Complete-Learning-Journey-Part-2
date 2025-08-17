aedf1bbaabc1115f30dbb6dfec7605b80daa0451
mirzayasirabdullahbaig07 <yasirabdullah4549@gmail.com>
2025-08-17 14:29:45 +0500

    What is Probability File Added

---
+++ 02_Maths/Probabililty/03_What_Is_Proabability.py
@@ -0,0 +1,7 @@
+# what is prpbability;
+# in simplest terms, the probabaiolity is the measure of the likelihood that a particulatr event wil occur. it is a fundamental concept in statistics and is used to make predictions and informed decisions in a wide range of discipline including science engineering medicine and social science
+# probability is usually expressed as a number between 0 and 1 inclusive
+
+#   a probability of 0 means that an event will not happen
+#  a probability of 1 means that an event will certainly happened
+#  a probability of 0.5 means that an event will happen half the time (or that is likely to happen as not to happen)
